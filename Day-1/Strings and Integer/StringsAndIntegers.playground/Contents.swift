f166280dbcdaea748f6b3dba588f29cdadecb9f2
Praveen Agarwal <praveen.agarwal@moveinsync.com>
2025-07-26 15:27:13 +0530

    wip

---
+++ Day-1/Strings and Integer/StringsAndIntegers.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+// Swift is a type safe language
+// means every variable must be of one specific type
+var greeting = "Hello, playground" // String
+var age = 38 // Int(integer)
+var population = 8_000_000 // _ helps to make numbers readable without changing their type
+
