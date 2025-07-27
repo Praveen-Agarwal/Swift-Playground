c22fe81f12a5ca668baba4846226fd47a04dd666
Praveen Agarwal <praveen.agarwal@moveinsync.com>
2025-07-27 23:17:14 +0530

    added day-4

---
+++ Day-4/loops.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+// For loop
+
+let count = 1...10
+
+for number in count {
+    print("Number is \(number)")
+}
+
+let albums = ["Red", "1989", "Reputation"]
+
+for album in albums {
+    print("\(album) is on Apple Music")
+}
+
+// use underscore if you are not using value in loop block of code
+print("Players gonna ")
+for _ in 1...5 {
+    print("play")
+}
