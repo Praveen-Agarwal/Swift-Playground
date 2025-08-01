8d02155ab2cf7c207d3bf966102c634e71bc1aa8
Praveen Agarwal <praveenagarwal0901@gmail.com>
2025-08-01 10:35:53 +0530

    closures-2 started

--- Day-7/closures-2.playground/Contents.swift
+++ Day-7/closures-2.playground/Contents.swift
@@ -1,1 +1,12 @@
 
+// Using closures as parameters when they accept parameters
+
+func travel(action: (String) -> Void) {
+    print("I'm getting ready to go.")
+    action("London")
+    print("I arrived!")
+}
+
+travel { (place: String) in
+    print("I'm traveling to \(place).")
+}
